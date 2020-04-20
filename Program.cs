9a6428f632474f51e056df46b6e9cdb7ef379246
Vivek Vijayarajan <vvijayarajan@njresources.com>
2020-04-20 18:56:58 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Packaging;
+
+namespace StoredProcToCSV
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.ReadLine();
+        }
+
+        private static void GetDataFromSqlStoredProc()
+        {
+            string outputFileName = @"C:\Temp\output.xlsx";
+            DataSet dsDataSet =  new DataSet();
+            using (SqlConnection conn = new SqlConnection("ConnectionString"))
+            {
+                SqlCommand sqlComm = new SqlCommand("ProcedureName", conn);
+                //sqlComm.Parameters.AddWithValue("@Start", StartTime);
+                //sqlComm.Parameters.AddWithValue("@Finish", FinishTime);
+                //sqlComm.Parameters.AddWithValue("@TimeRange", TimeRange);
+
+                sqlComm.CommandType = CommandType.StoredProcedure;
+                SqlDataAdapter da = new SqlDataAdapter();
+                da.SelectCommand = sqlComm;
+                da.Fill(dsDataSet);
+
+                ExportDataSet(dsDataSet, outputFileName);
+            }
+
+        }
+
+        private static void GetDataFromSqlStatement()
+        {
+            string outputFileName = @"C:\Temp\output.xlsx";
+            var connectionString = ConfigurationManager.ConnectionStrings["SQLConnection"].ConnectionString;
+            string queryString = "SELECT *  FROM [dbo].[Exception_Detail]";
+
+
+            using (SqlConnection connection = new SqlConnection(
+              connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Connection.Open();
+                SqlDataAdapter da = new SqlDataAdapter();
+                da.SelectCommand = command;
+                DataSet data = new DataSet();
+                da.Fill(data);
+
+                ExportDataSet(data, outputFileName);
+            }
+        }
+
+
+        private static void ExportDataSet(DataSet ds, string destination)
+        {
+            using (var workbook = SpreadsheetDocument.Create(destination, DocumentFormat.OpenXml.SpreadsheetDocumentType.Workbook))
+            {
+                var workbookPart = workbook.AddWorkbookPart();
+
+                workbook.WorkbookPart.Workbook = new DocumentFormat.OpenXml.Spreadsheet.Workbook();
+
+                workbook.WorkbookPart.Workbook.Sheets = new DocumentFormat.OpenXml.Spreadsheet.Sheets();
+
+                foreach (System.Data.DataTable table in ds.Tables)
+                {
+
+                    var sheetPart = workbook.WorkbookPart.AddNewPart<WorksheetPart>();
+                    var sheetData = new DocumentFormat.OpenXml.Spreadsheet.SheetData();
+                    sheetPart.Worksheet = new DocumentFormat.OpenXml.Spreadsheet.Worksheet(sheetData);
+
+                    DocumentFormat.OpenXml.Spreadsheet.Sheets sheets = workbook.WorkbookPart.Workbook.GetFirstChild<DocumentFormat.OpenXml.Spreadsheet.Sheets>();
+                    string relationshipId = workbook.WorkbookPart.GetIdOfPart(sheetPart);
+
+                    uint sheetId = 1;
+                    if (sheets.Elements<DocumentFormat.OpenXml.Spreadsheet.Sheet>().Count() > 0)
+                    {
+                        sheetId =
+                            sheets.Elements<DocumentFormat.OpenXml.Spreadsheet.Sheet>().Select(s => s.SheetId.Value).Max() + 1;
+                    }
+
+                    DocumentFormat.OpenXml.Spreadsheet.Sheet sheet = new DocumentFormat.OpenXml.Spreadsheet.Sheet() { Id = relationshipId, SheetId = sheetId, Name = table.TableName };
+                    sheets.Append(sheet);
+
+                    DocumentFormat.OpenXml.Spreadsheet.Row headerRow = new DocumentFormat.OpenXml.Spreadsheet.Row();
+
+                    List<String> columns = new List<string>();
+                    foreach (System.Data.DataColumn column in table.Columns)
+                    {
+                        columns.Add(column.ColumnName);
+
+                        DocumentFormat.OpenXml.Spreadsheet.Cell cell = new DocumentFormat.OpenXml.Spreadsheet.Cell();
+                        cell.DataType = DocumentFormat.OpenXml.Spreadsheet.CellValues.String;
+                        cell.CellValue = new DocumentFormat.OpenXml.Spreadsheet.CellValue(column.ColumnName);
+                        headerRow.AppendChild(cell);
+                    }
+
+
+                    sheetData.AppendChild(headerRow);
+
+                    foreach (System.Data.DataRow dsrow in table.Rows)
+                    {
+                        DocumentFormat.OpenXml.Spreadsheet.Row newRow = new DocumentFormat.OpenXml.Spreadsheet.Row();
+                        foreach (String col in columns)
+                        {
+                            DocumentFormat.OpenXml.Spreadsheet.Cell cell = new DocumentFormat.OpenXml.Spreadsheet.Cell();
+                            cell.DataType = DocumentFormat.OpenXml.Spreadsheet.CellValues.String;
+                            cell.CellValue = new DocumentFormat.OpenXml.Spreadsheet.CellValue(dsrow[col].ToString()); //
+                            newRow.AppendChild(cell);
+                        }
+
+                        sheetData.AppendChild(newRow);
+                    }
+
+                }
+            }
+        }
+    }
+}
